f9c10e41ef3065ccd090bf59ba7ab72512c40e16
kaczor6418 <andrzej6418@gmail.com>
2018-04-18 00:22:42 +0200

    save data beginnings

--- js/validation_form.js
+++ js/validation_form.js
@@ -1,3 +1,9 @@
+var data = {
+  compleated: [],
+  todo: [],
+  priorities: []
+};
+
 (function() {
 
 var form = document.querySelector("#validation"),
@@ -6,16 +12,16 @@ var form = document.querySelector("#validation"),
     selectPriority = form.querySelector("select");
 
 // SVG that will be used into TODOlist
-var isDone = '<svg class="checkBox" width="30" height="30" xmlns="http://www.w3.org/2000/svg"> <g><title>background</title><rect fill="none" height="402" width="582" y="-1" x="-1"/> </g><g><title>Layer 1</title><rect rx="2" fill-opacity="0" height="23.022598" width="22.17514" y="3.453391" x="3.947742" stroke-width="1.5" stroke="#b6b6b6" fill="#0fffff"/><path fill="#e79c49" opacity="0" d="m24,4l-18,0c-1.105,0 -2,0.895 -2,2l0,18c0,1.105 0.895,2 2,2l18,0c1.105,0 2,-0.895 2,-2l0,-18c0,-1.105 -0.896,-2 -2,-2zm-2.293,7.707l-7.56,7.56c-0.188,0.188 -0.442,0.293 -0.707,0.293s-0.52,-0.105 -0.707,-0.293l-3.453,-3.453c-0.391,-0.391 -0.391,-1.023 0,-1.414s1.023,-0.391 1.414,0l2.746,2.746l6.853,-6.853c0.391,-0.391 1.023,-0.391 1.414,0s0.391,1.023 0,1.414z"/></g></svg>',
+var isDone = '<svg class="checkBox" width="30" height="30" xmlns="http://www.w3.org/2000/svg"> <g><title>background</title><rect fill="none" height="402" width="582" y="-1" x="-1"/> </g><g><title>Layer 1</title><rect rx="2" fill-opacity="0" height="23.022598" width="22.17514" y="3.453391" x="3.947742" stroke-width="1.5" stroke="#b6b6b6" fill="#0fffff"/><path class="todo" fill="#e79c49" opacity="0" d="m24,4l-18,0c-1.105,0 -2,0.895 -2,2l0,18c0,1.105 0.895,2 2,2l18,0c1.105,0 2,-0.895 2,-2l0,-18c0,-1.105 -0.896,-2 -2,-2zm-2.293,7.707l-7.56,7.56c-0.188,0.188 -0.442,0.293 -0.707,0.293s-0.52,-0.105 -0.707,-0.293l-3.453,-3.453c-0.391,-0.391 -0.391,-1.023 0,-1.414s1.023,-0.391 1.414,0l2.746,2.746l6.853,-6.853c0.391,-0.391 1.023,-0.391 1.414,0s0.391,1.023 0,1.414z"/></g></svg>',
     deleTask = '<svg class="delete" width="12.7" height="12.7" xmlns="http://www.w3.org/2000/svg"> <g> <title>background</title> <rect fill="none" height="402" width="582" y="-1" x="-1"/> </g> <g> <title>Layer 1</title> <path clip-rule="nonzero" fill-rule="evenodd" stroke-width="1.411111" stroke-miterlimit="4" stroke-dashoffset="0" d="m3.88056,2.822212l-1.05834,1.05834l2.46945,2.46944l-2.46945,2.46944l1.05834,1.05834l2.46944,-2.46945l2.46944,2.46945l1.05834,-1.05834l-2.46945,-2.46944l2.46945,-2.46944l-1.05834,-1.05834l-2.46944,2.46945l-2.46944,-2.46945z"/> </g></svg>';
 
 // Adds a new item to the TODOlist
 function addActivity() {
   var activityTable = document.querySelector("tbody"),
+      row = activityTable.insertRow(0),
       counter = true;
   let activity = enterActivity.value,
       priority = selectPriority.value,
-      row = activityTable.insertRow(0),
       cell1 = row.insertCell(0),
       cell2 = row.insertCell(1),
       cell3 = row.insertCell(2);
@@ -31,13 +37,13 @@ function addActivity() {
 
 // Add click event for change the state of item(whether the task is done or not)
       cell3.addEventListener("click", function (e) {
-        if(counter){
-          cell3.querySelector("path").style.opacity = "1";
-          counter = false;
-        } else {
-          cell3.querySelector("path").style.opacity = "0";
-          counter = true;
-        }
+          if(counter){
+            cell3.querySelector(".todo").classList.add("done");
+            counter = false;
+          } else {
+            cell3.querySelector(".todo").classList.remove("done");
+            counter = true;
+          }
       },false);
 
 // Add click event for removing item(whether the task is done or not)
@@ -51,6 +57,9 @@ function addActivity() {
       cell2.innerHTML = priority;
       cell3.innerHTML = isDone;
       cell4.innerHTML = deleTask;
+
+      data.todo.push(activity);
+      data.priorities.push(priority);
 }
 
 function isEmpty(field) {
