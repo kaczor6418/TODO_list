8b961ef709517b13fe6a4e57b9f3b41e58f82864
kaczor6418 <andrzej6418@gmail.com>
2018-04-18 15:15:54 +0200

    add all activities that user added to the local storage(data object)

--- js/validation_form.js
+++ js/validation_form.js
@@ -1,8 +1,8 @@
-var data = {
-  compleated: [],
-  todo: [],
-  priorities: []
+var data = (localStorage.getItem("todoList")) ?  JSON.parse(localStorage.getItem("todoList")):{
+  completed: [],
+  todo: []
 };
+renderTodoList();
 
 (function() {
 
@@ -12,16 +12,26 @@ var form = document.querySelector("#validation"),
     selectPriority = form.querySelector("select");
 
 // SVG that will be used into TODOlist
-var isDone = '<svg class="checkBox" width="30" height="30" xmlns="http://www.w3.org/2000/svg"> <g><title>background</title><rect fill="none" height="402" width="582" y="-1" x="-1"/> </g><g><title>Layer 1</title><rect rx="2" fill-opacity="0" height="23.022598" width="22.17514" y="3.453391" x="3.947742" stroke-width="1.5" stroke="#b6b6b6" fill="#0fffff"/><path class="todo" fill="#e79c49" opacity="0" d="m24,4l-18,0c-1.105,0 -2,0.895 -2,2l0,18c0,1.105 0.895,2 2,2l18,0c1.105,0 2,-0.895 2,-2l0,-18c0,-1.105 -0.896,-2 -2,-2zm-2.293,7.707l-7.56,7.56c-0.188,0.188 -0.442,0.293 -0.707,0.293s-0.52,-0.105 -0.707,-0.293l-3.453,-3.453c-0.391,-0.391 -0.391,-1.023 0,-1.414s1.023,-0.391 1.414,0l2.746,2.746l6.853,-6.853c0.391,-0.391 1.023,-0.391 1.414,0s0.391,1.023 0,1.414z"/></g></svg>',
+var isDone = '<svg class="checkBox" width="30" height="30" xmlns="http://www.w3.org/2000/svg"> <g><title>background</title><rect fill="none" height="402" width="582" y="-1" x="-1"/> </g><g><title>Layer 1</title><rect rx="2" fill-opacity="0" height="23.022598" width="22.17514" y="3.453391" x="3.947742" stroke-width="1.5" stroke="#b6b6b6" fill="#0fffff"/><path fill="#e79c49" opacity="0" d="m24,4l-18,0c-1.105,0 -2,0.895 -2,2l0,18c0,1.105 0.895,2 2,2l18,0c1.105,0 2,-0.895 2,-2l0,-18c0,-1.105 -0.896,-2 -2,-2zm-2.293,7.707l-7.56,7.56c-0.188,0.188 -0.442,0.293 -0.707,0.293s-0.52,-0.105 -0.707,-0.293l-3.453,-3.453c-0.391,-0.391 -0.391,-1.023 0,-1.414s1.023,-0.391 1.414,0l2.746,2.746l6.853,-6.853c0.391,-0.391 1.023,-0.391 1.414,0s0.391,1.023 0,1.414z"/></g></svg>',
     deleTask = '<svg class="delete" width="12.7" height="12.7" xmlns="http://www.w3.org/2000/svg"> <g> <title>background</title> <rect fill="none" height="402" width="582" y="-1" x="-1"/> </g> <g> <title>Layer 1</title> <path clip-rule="nonzero" fill-rule="evenodd" stroke-width="1.411111" stroke-miterlimit="4" stroke-dashoffset="0" d="m3.88056,2.822212l-1.05834,1.05834l2.46945,2.46944l-2.46945,2.46944l1.05834,1.05834l2.46944,-2.46945l2.46944,2.46945l1.05834,-1.05834l-2.46945,-2.46944l2.46945,-2.46944l-1.05834,-1.05834l-2.46944,2.46945l-2.46944,-2.46945z"/> </g></svg>';
 
-// Adds a new item to the TODOlist
+function renderTodoList() {
+
+}
+
+// Add activities to the localStorage
+function dataObjectUpdate() {
+  localStorage.setItem("todoList", JSON.stringify(data));
+}
+
+// Adds a new activity to the TODOlist
 function addActivity() {
   var activityTable = document.querySelector("tbody"),
       row = activityTable.insertRow(0),
       counter = true;
   let activity = enterActivity.value,
       priority = selectPriority.value,
+      item = activity + " " + priority,
       cell1 = row.insertCell(0),
       cell2 = row.insertCell(1),
       cell3 = row.insertCell(2);
@@ -35,42 +45,68 @@ function addActivity() {
           selectPriority.value = "";
         }
 
-// Add click event for change the state of item(whether the task is done or not)
+// Add click event for change the state of activity(whether the task is done or not)
       cell3.addEventListener("click", function (e) {
           if(counter){
-            cell3.querySelector(".todo").classList.add("done");
+
+            // Save state of item in data
+            data.todo.splice(data.todo.indexOf(item), 1);
+            data.completed.push(item);
+
+            // Change the state of activity
+            cell3.querySelector("path").style.opacity = "1";
             counter = false;
           } else {
-            cell3.querySelector(".todo").classList.remove("done");
+            // Save the state of activity in data
+            data.completed.splice(data.completed.indexOf(item), 1);
+            data.todo.push(item);
+
+            // Change the state of activity
+            cell3.querySelector("path").style.opacity = "0";
             counter = true;
           }
+          dataObjectUpdate();
       },false);
 
-// Add click event for removing item(whether the task is done or not)
+// Add click event for removing activity(whether the task is done or not)
       cell4.addEventListener("click", function (e) {
         let toRemove = this.parentNode;
 
+        if(counter){
+
+          // Delete activity from data.completed
+          data.todo.splice(data.todo.indexOf(item), 1);
+        } else {
+          // Delete activity from data.todo
+          data.completed.splice(data.completed.indexOf(item), 1);
+        }
+        dataObjectUpdate();
         activityTable.removeChild(toRemove);
       },false);
 
+// Add new activity to the website
       cell1.innerHTML = activity;
       cell2.innerHTML = priority;
       cell3.innerHTML = isDone;
       cell4.innerHTML = deleTask;
 
-      data.todo.push(activity);
-      data.priorities.push(priority);
+// Save activity and priority in data
+      data.todo.push(item);
+      dataObjectUpdate();
 }
 
+// Checks the form field is empty
 function isEmpty(field) {
   return field.value !== "";
 }
 
+// Add click event for add activity
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   let errors = [];
 
+// Checks the form has been completed
   for (let i = 0; i < fields.length; i++) {
     let field = fields[i],
         isValid = null;
@@ -97,10 +133,7 @@ form.addEventListener("submit", function (e) {
     selectPriority.style.background = "#ffc6c6";
   }else {
     addActivity();
-    //form.submit();
   }
-
-console.log(errors);
 
 }, false)
 })();
