8c29220b86a232983f78a73102f4c86199b900bf
kaczor6418 <andrzej6418@gmail.com>
2018-04-18 15:59:05 +0200

    render todo list beginnings

--- js/validation_form.js
+++ js/validation_form.js
@@ -2,7 +2,6 @@ var data = (localStorage.getItem("todoList")) ?  JSON.parse(localStorage.getItem
   completed: [],
   todo: []
 };
-renderTodoList();
 
 (function() {
 
@@ -15,8 +14,24 @@ var form = document.querySelector("#validation"),
 var isDone = '<svg class="checkBox" width="30" height="30" xmlns="http://www.w3.org/2000/svg"> <g><title>background</title><rect fill="none" height="402" width="582" y="-1" x="-1"/> </g><g><title>Layer 1</title><rect rx="2" fill-opacity="0" height="23.022598" width="22.17514" y="3.453391" x="3.947742" stroke-width="1.5" stroke="#b6b6b6" fill="#0fffff"/><path fill="#e79c49" opacity="0" d="m24,4l-18,0c-1.105,0 -2,0.895 -2,2l0,18c0,1.105 0.895,2 2,2l18,0c1.105,0 2,-0.895 2,-2l0,-18c0,-1.105 -0.896,-2 -2,-2zm-2.293,7.707l-7.56,7.56c-0.188,0.188 -0.442,0.293 -0.707,0.293s-0.52,-0.105 -0.707,-0.293l-3.453,-3.453c-0.391,-0.391 -0.391,-1.023 0,-1.414s1.023,-0.391 1.414,0l2.746,2.746l6.853,-6.853c0.391,-0.391 1.023,-0.391 1.414,0s0.391,1.023 0,1.414z"/></g></svg>',
     deleTask = '<svg class="delete" width="12.7" height="12.7" xmlns="http://www.w3.org/2000/svg"> <g> <title>background</title> <rect fill="none" height="402" width="582" y="-1" x="-1"/> </g> <g> <title>Layer 1</title> <path clip-rule="nonzero" fill-rule="evenodd" stroke-width="1.411111" stroke-miterlimit="4" stroke-dashoffset="0" d="m3.88056,2.822212l-1.05834,1.05834l2.46945,2.46944l-2.46945,2.46944l1.05834,1.05834l2.46944,-2.46945l2.46944,2.46945l1.05834,-1.05834l-2.46945,-2.46944l2.46945,-2.46944l-1.05834,-1.05834l-2.46944,2.46945l-2.46944,-2.46945z"/> </g></svg>';
 
+renderTodoList();
+
+// Render TODO LIST of all activities that user added to the taskTable
 function renderTodoList() {
 
+    // If all elements of the data object are empty, the function won't render anything
+  if (!data.todo.length && !data.completed.length) return;
+
+  for (let i = 0; i < data.completed.length; i++) {
+    let value = data.completed[i];
+    console.log(value);
+    console.log(data.completed.length);
+  }
+  for (let i = 0; i < data.todo.length; i++) {
+    let value = data.todo[i];
+    console.log(value);
+    console.log(data.completed.length);
+  }
 }
 
 // Add activities to the localStorage
@@ -25,13 +40,12 @@ function dataObjectUpdate() {
 }
 
 // Adds a new activity to the TODOlist
-function addActivity() {
+function addActivity(activity, priority) {
   var activityTable = document.querySelector("tbody"),
       row = activityTable.insertRow(0),
       counter = true;
-  let activity = enterActivity.value,
-      priority = selectPriority.value,
-      item = activity + " " + priority,
+
+  let item = activity + " " + priority,
       cell1 = row.insertCell(0),
       cell2 = row.insertCell(1),
       cell3 = row.insertCell(2);
@@ -132,7 +146,9 @@ form.addEventListener("submit", function (e) {
     enterActivity.style.background = "#ffc6c6";
     selectPriority.style.background = "#ffc6c6";
   }else {
-    addActivity();
+    var taskName = enterActivity.value,
+        taskPriority = selectPriority.value;
+    addActivity(taskName,taskPriority);
   }
 
 }, false)
